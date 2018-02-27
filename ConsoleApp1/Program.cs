54424c583a49ece084dcc1600c77ea97086e5dd7
Emelman <emelman331@gmail.com>
2018-02-27 17:26:16 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int i;
+
+            for(i=0; i < 20; i++)
+            {
+                Console.WriteLine("boolshit"+i.ToString());
+            }
+
+            Console.WriteLine("boolshit2!");
+            Console.WriteLine("boolshit3!");
+            
+
+
+        }
+    }
+}
