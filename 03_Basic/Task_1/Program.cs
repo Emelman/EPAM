1c2ac1b11e52ce7ca208a568ced922df4737736d
RayStoun <Stigmatory3@gmail.com>
2018-03-02 10:28:23 +0300

    start new Solution

---
+++ 03_Basic/Task_1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    class Program
+    {
+
+        static Random randomNum = new Random();
+        
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter array length:");
+            int[] numArray = NumGenerator((int.Parse(Console.ReadLine())));
+
+
+        }
+
+        private static int[] NumGenerator(int length = 0, int min = -10, int max = 10)
+        {
+            int[] tempArray = new int[length];
+            for(var i = 0; i < length; i++)
+            {
+                tempArray[i] = randomNum.Next(min, max);
+            }
+
+
+            return tempArray;
+        }
+
+    }
+}
