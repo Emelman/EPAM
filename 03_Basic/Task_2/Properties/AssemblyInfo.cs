b432b45a90e93c7e0c22a25b2d755e219488693b
Emelman <emelman331@gmail.com>
2018-03-04 19:37:48 +0300

    put everything in order

--- 03_Basic/Task_2/Properties/AssemblyInfo.cs
+++ 03_Basic/Task_2/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("03_Basic")]
+[assembly: AssemblyTitle("Task_2")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("EPAM Systems")]
-[assembly: AssemblyProduct("03_Basic")]
-[assembly: AssemblyCopyright("Copyright © EPAM Systems 2018")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Task_2")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("0e588f63-95c5-41b2-925e-01c814c72f0f")]
+[assembly: Guid("01821e53-b093-44c5-b284-b44d57caca54")]
 
 // Version information for an assembly consists of the following four values:
 //
