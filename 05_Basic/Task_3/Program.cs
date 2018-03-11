5f1ecd6b41a4e7633b5015e93d53186543426646
Emelman <emelman331@gmail.com>
2018-03-11 20:46:53 +0300

    Task 3 finished

---
+++ 05_Basic/Task_3/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter three sides of the triangle, use space or letter as a separator:");
+            Mark: string sides = Console.ReadLine();
+            string[] parameters = SeparateOnWords(sides);
+            if (parameters.Length < 3)
+            {
+                Console.WriteLine("Error! Wrong DATA!!!");
+                goto Mark;
+            }
+            else
+            {
+                GenerateTriangle(parameters);
+                
+            }
+        }
+
+        private static void GenerateTriangle(string[] parameters)
+        {
+            try
+            {
+                Triangle litleAng = new Triangle(int.Parse(parameters[0]), int.Parse(parameters[1]), int.Parse(parameters[2]));
+                if (litleAng != null)
+                {
+                    litleAng.PrintSides();
+                }
+                else
+                {
+                    throw new Exception("There is no triangle with such sides");
+                }
+
+                double ar = litleAng.Area();
+
+                Console.WriteLine("Perimetr={0}", litleAng.Perimetr());
+                Console.WriteLine("Sqaure={0}", litleAng.Area());
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+
+        }
+
+        private static string[] SeparateOnWords(string text)
+        {
+            List<char> separatorArray = new List<char>();
+            for (var i = 0; i < text.Length; i++)
+            {
+                if (!Char.IsDigit(text[i]))
+                {
+                    separatorArray.Add(text[i]);
+                }
+            }
+            string[] words = text.Split(separatorArray.ToArray(), StringSplitOptions.RemoveEmptyEntries);
+            return words;
+        }
+    }
+}
---
+++ 05_Basic/Task_3/Triangle.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3
+{
+    class Triangle
+    {
+        private Boolean initialised = false;
+        private int sideA;
+        private int sideB;
+        private int sideC;
+        private int area;
+        private int perimetr;
+
+
+        public Triangle(int v1, int v2, int v3)
+        {
+            IsValid(v1, v2, v3);
+            SideA = v1;
+            SideB = v2;
+            SideC = v3;
+            initialised = true;
+        }
+
+        public int SideA
+        {
+            get
+            {
+                return sideA;
+            }
+            set
+            {
+                if (initialised)
+                {
+                    IsValid();
+                }
+                sideA = value;
+            }
+        }
+
+        public int SideB
+        {
+            get
+            {
+                return sideB;
+            }
+            set
+            {
+                if (initialised)
+                {
+                    IsValid();
+                }
+                sideB = value;
+            }
+        }
+
+        public int SideC
+        {
+            get
+            {
+                return sideC;
+            }
+            set
+            {
+                if (initialised)
+                {
+                    IsValid();
+                }
+                sideC = value;
+            }
+        }
+
+        internal void PrintSides()
+        {
+            Console.WriteLine("a is {0}, b is {1}, c is {2}", sideA, sideB, sideC);
+            Console.ReadKey();
+        }
+
+        static void IsValid(int a, int b, int c)
+        {
+            if (a <= 0 || b <= 0 || c <= 0)
+            {
+                throw new Exception("Sides of the triangle can` t be negative");
+            }
+            else if (a + b > c || a + c > b || b + c > a)
+            {
+                throw new Exception("This is not a triangle");
+            }
+        }
+
+        private void IsValid()
+        {
+            if ((sideA <= 0 || sideB <= 0 || sideC <= 0) && isTriangle) 
+            {
+                throw new Exception("Sides of the triangle can` t be negative");
+            }
+        }
+
+        private bool isTriangle
+        {
+            get
+            {
+                if (sideA + sideB > sideC && sideA + sideC > sideB && sideB + sideC > sideA)
+                {
+                    return true;
+                }
+                return false;
+            }
+        }
+
+        public int Perimetr()
+        {
+            return sideA + sideB + sideC;
+        }
+
+        public double Area()
+        {
+            return Math.Sqrt(HalfPer() * (HalfPer() - sideA) * (HalfPer() - sideB) * (HalfPer() - sideC));
+        }
+
+        public double HalfPer()
+        {
+            return Perimetr() / 2;
+        }
+    }
+}
