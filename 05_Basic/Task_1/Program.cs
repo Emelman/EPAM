b7a4b639db8782e32bdac14a09bab9aa7b5f3f4f
Emelman <emelman331@gmail.com>
2018-03-11 17:06:09 +0300

    First task from basic course 5 finished

---
+++ 05_Basic/Task_1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Options();
+            
+
+        }
+
+        private static void Options()
+        {
+            Console.WriteLine("Create new user, enter his name:");
+            string userName = Console.ReadLine();
+
+            string dateString = "5/1/2008 8:30:52 AM";
+            DateTime date1 = DateTime.Parse(dateString, CultureInfo.InvariantCulture);
+            User someUser = CreateNewAlex(userName, "Borisovich", "Dromov", date1);
+            Console.WriteLine("Show generated user data:");
+            Console.ReadKey();
+
+            Console.WriteLine(someUser.BirthDay);
+            Console.WriteLine(someUser.Name);
+            Console.WriteLine(someUser.Surname);
+            Console.WriteLine(someUser.Patronomic);
+            Console.WriteLine(someUser.Age);
+            Console.ReadKey();
+        }
+
+        private static User CreateNewAlex(string name, string patronymic, string surname, DateTime date1)
+        {
+            User alex = new User();
+            alex.Name = name;
+            alex.Surname = surname;
+            alex.Patronomic = patronymic;
+            alex.BirthDay = date1;
+            return alex; //brand new alex
+        }
+    }
+}
---
+++ 05_Basic/Task_1/User.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    class User
+    {
+        string name;
+        string surname;
+        string patronymic;
+        DateTime birthDay;
+        int age;
+
+        public string Name
+        {
+            get
+            {
+                if(name == string.Empty)
+                {
+                    throw (Error.InnerException);
+                }
+                return name;
+            }
+            set
+            {
+                if(value != string.Empty)
+                {
+                    name = value;
+                }
+                else
+                {
+                    throw (Error.InnerException);
+                }
+            }
+        }
+
+        public string Surname
+        {
+            get
+            {
+                if (surname == string.Empty)
+                {
+                    throw (Error.InnerException);
+                }
+                return surname;
+            }
+            set
+            {
+                if (value != string.Empty)
+                {
+                    surname = value;
+                }
+                else
+                {
+                    throw (Error.InnerException);
+                }
+            }
+        }
+
+        public string Patronomic
+        {
+            get
+            {
+                return patronymic;
+            }
+            set
+            {
+                if(value != string.Empty)
+                {
+                    patronymic = value;
+                }
+                else
+                {
+                    throw (Error.InnerException);
+                }
+            }
+        }
+
+        public DateTime BirthDay
+        {
+            get
+            {
+                return birthDay;
+            }
+            set
+            {
+                if(value == null)
+                {
+                    throw (Error.InnerException);
+                }
+                else
+                {
+                    birthDay = value;
+                    DateTime current = DateTime.Today;
+                    Age = current.Year - value.Year;
+                }
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                if(age < 0)
+                {
+                    throw (Error.InnerException);
+                }
+                else
+                {
+                    age = value;
+                }
+            }
+        }
+
+        public Exception Error { get; private set; }
+    }
+}
