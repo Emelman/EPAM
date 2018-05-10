df2ffd0534ca8a0b329fe8927b1fee3587157cf3
Leonard Vid <emelman331@Gmail.com>
2018-05-10 11:27:27 +0300

    last asp net

---
+++ UsersAndRewardsNew/UsersAndRewards.PL.Web/Controllers/RewardsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UsersAndRewards.BL.Logic;
+using UsersAndRewards.Shared;
+
+namespace UsersAndRewards.PL.Web.Controllers
+{
+    public class RewardsController : Controller
+    {
+        // GET: Rewards
+        ILogic logik = new Logic();
+        public ActionResult Index()
+        {
+            return View("Index",logik.GetRewardViewModels());
+        }
+
+        public ActionResult Create()
+        {
+            return View("Create");
+        }
+
+        public ActionResult SubmitReward(Reward reward)
+        {
+            logik.AddReward(reward);
+            return RedirectToAction("Index","Users");
+        }
+    }
+}
---
+++ UsersAndRewardsNew/UsersAndRewards.PL.Web/Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UsersAndRewards.BL.Logic;
+using UsersAndRewards.Shared;
+
+namespace UsersAndRewards.PL.Web.Controllers
+{
+    public class UsersController : Controller
+    {
+        ILogic logic = new Logic();
+
+        // GET: Users
+        public ActionResult Index()
+        {
+            return View(logic.GetUsersViewModel());
+        }
+
+        public ActionResult Create()
+        {
+            return View("Create");
+        }
+
+        public ActionResult AddUser(User user)
+        {
+            logic.AddUser(user);
+            return RedirectToAction("Index","Rewards");
+        }
+    }
+}
